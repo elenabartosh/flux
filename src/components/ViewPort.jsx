c92bb535764658d0df47e412df2cfb181931fdc7
Elena Bartosh <elena.karnaukhova@gmail.com>
2017-12-20 15:52:32 -0800

    Clean up

--- src/components/ViewPort.jsx
+++ src/components/ViewPort.jsx
@@ -5,9 +5,6 @@ import { Button, Form } from 'semantic-ui-react'
 class ViewPort extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      view: false
-    };
   }
   
   componentWillReceiveProps ({ isScriptLoaded, isScriptLoadSucceed }) {
@@ -31,33 +28,9 @@ class ViewPort extends React.Component {
   }
 
   initViewPort() {
-    console.log('initViewPort');
-    console.log(document.getElementById('view'));
     var viewport = new window.FluxViewport(document.querySelector("#view"));
     // set up default lighting for the viewport
     viewport.setupDefaultLighting();
-    console.log(viewport);
-    var box_data = [
-    {
-    "dimensions": [
-      2,
-      2,
-      2
-    ],
-    "origin": [
-      0,
-      0,
-      0
-    ],
-    "primitive": "block",
-    "units": {
-      "dimensions": "meters",
-      "origin": "meters"
-    }
-    }
-    ]
-    viewport.setGeometryEntity(box_data);
-    this.setState({view: true});
   }
 
   render() {
