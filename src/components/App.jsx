665a68142b2ee20430f419430da68008565a8210
Elena Bartosh <elena.karnaukhova@gmail.com>
2017-12-21 17:06:54 -0800

    Add cell selector for project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,8 +14,11 @@ class App extends Component {
     this.state = {
       user: undefined,
       userLoggedIn: false,
-      userProjects: []
+      userProjects: [],
+      projectCells: []
     };
+    this.dataTables = {}
+    this.handleProjectChange = this.handleProjectChange.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
   }
@@ -53,36 +56,82 @@ class App extends Component {
     this.setState({userLoggedIn: false, user: undefined, userProjects: []});
   }
 
+  handleProjectChange(event) {
+    console.log('handleProjectChange');
+    var filteredProjects = this.state.userProjects.filter((p) => { return p.id === event.target.value });
+    if (filteredProjects.length > 0) {
+      var selectedProject = filteredProjects[0];
+      this.getCells(selectedProject)
+      .then((data) => {
+        this.setState({projectCells: data.entities});
+      })
+    }
+  }
+
+  getUser() {
+    return this.state.user;
+  }
+
+  getDataTable(project) {
+    if (!(project.id in this.dataTables)) {
+      var user = this.getUser();
+      if (user !== undefined) {
+        var dt = user.getDataTable(project.id);
+        this.dataTables[project.id] = { table: dt, handlers: {}, websocketOpen: false };
+      } else {
+        return {};
+      }
+    }
+    return this.dataTables[project.id];
+  }
+
+  getCells(project) {
+    return this.getDataTable(project).table.listCells();
+  }
+
   render() {
     const userLoggedIn = this.state.userLoggedIn;
     console.log('render userLoggedIn', userLoggedIn);
-    return (<div className='App'>{userLoggedIn ? (
-              <div className='App'>
-                <div id='header'>
-                  <div id='title'>
-                    <h1>FLUX</h1>
-                    <h2>Seed Project</h2>
-                  </div>
-                <div id='actions'>
-                  <div className='select'><select className='project'>
+    return (
+      <div className='App'>
+        { userLoggedIn ? (
+          <div className='App'>
+            <div id='header'>
+              <div id='title'>
+                <h1>FLUX</h1>
+                <h2>Seed Project</h2>
+              </div>
+            <div id='actions'>
+              <div className='select'>
+                <select className='project' onChange={this.handleProjectChange}>
                   <option key='notselected' value='notselected'>Please select a project</option>
                   {this.state.userProjects.map((item) => {
                     return <option key={item.id} value={item.id}>{item.name}</option>
                   })}
-                  </select></div>
-                  <div id='logout'><button onClick={this.handleLogout}>Logout</button></div>
-                </div>
+                </select></div>
+              <div id='logout'>
+                <button onClick={this.handleLogout}>Logout</button>
               </div>
-              <div id='viewport'><ViewPort/></div>
             </div>
-          ) : (
-            <div><Login handleLogin={this.handleLogin} /></div>
+          </div>
+          <div id='viewport'><ViewPort/></div>
+          <div className='select'>
+            <select className='cell'>
+              <option key='notselected' value='notselected'>Please select a cell</option>
+              {this.state.projectCells.map((cell) => {
+                return <option key={cell.id} value={cell.id}>{cell.label}</option>
+              })}
+            </select></div>
+          </div>
+        ) : (
+          <div>
+            <Login handleLogin={this.handleLogin} />
+          </div>
         )}
-        </div>
+      </div>
     );
   }
 }
-
 
 export default App;
 
